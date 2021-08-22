128815d5afda2c792f58cf514d8722045bc05436
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-08-22 16:21:38 +0300

    Added playersprite and player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D playerRigidBody;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement);
+    }
+}
