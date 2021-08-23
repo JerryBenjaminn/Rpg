9ac2cc5b782c5440c4765fa606d923b3201ddbc5
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-08-23 19:04:16 +0300

    Added a canvas and made a fading animation for it. Made a prefab and a script to duplicate the effect on all scenes

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -24,7 +24,18 @@ public class AreaExit : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Player.instance.transitionName = transitionAreaName;
-            SceneManager.LoadScene(sceneToLoad);
+
+            MenuManager.instance.FadeImage();
+
+            StartCoroutine(LoadSceneCoroutine());
         }
     }
+
+    IEnumerator LoadSceneCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
 }
