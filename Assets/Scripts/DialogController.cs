d971b1d610380534f933f636c5bfe60d6cee68c6
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-08-23 20:46:56 +0300

    Added dialog UI and made some basic dialog lines.

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI dialogText, nameText;
+    [SerializeField] GameObject dialogBox, nameBox;
+
+    [SerializeField] string [] dialogSentences;
+    [SerializeField] int currentSentence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogText.text = dialogSentences[currentSentence];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
