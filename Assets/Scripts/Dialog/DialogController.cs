5bab7a63125db93deca9576924a9cf1ccb760f1b
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-08-23 23:15:13 +0300

    Added an npc and created a script to trigger a dialog when entering near the npc

--- Assets/Scripts/Dialog/DialogController.cs
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -12,15 +12,34 @@ public class DialogController : MonoBehaviour
     [SerializeField] string [] dialogSentences;
     [SerializeField] int currentSentence;
 
+    public static DialogController instance;
+
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         dialogText.text = dialogSentences[currentSentence];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                currentSentence++;
+
+                if(currentSentence >= dialogSentences.Length)
+                {
+                    dialogBox.SetActive(false);
+                }
+                else
+                {
+                    dialogText.text = dialogSentences[currentSentence];
+                }
+
+            }
+        }
     }
 }
