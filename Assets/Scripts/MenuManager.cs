e5fb4612562ddbd9434b0c88d16e7a902ed76c74
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-09-18 18:07:17 +0300

    Adjusted the characters movementspeed

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,16 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] Image imageToFade;
+    [SerializeField] GameObject menu;
 
     public static MenuManager instance;
 
+    private PlayerStats[] playerStats;
+    [SerializeField] TextMeshProUGUI[] nameText, hpText, manaText, lvlText, xpText;
+    [SerializeField] Slider[] xpSlider;
+    [SerializeField] Image[] characterImage;
+    [SerializeField] GameObject[] characterPanel;
     private void Start()
     {
         instance = this;   
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.M)) // Tarkistetaan jos painetaan M-näppäintä, onko menu aktiivisena näytöllä. Jos ei, niin se aktivoidaan
+        {
+            if (menu.activeInHierarchy)
+            {
+                menu.SetActive(false);
+            }
+            else
+            {
+                menu.SetActive(true);
+            }
+        }      
     }
     public void FadeImage()
     {
